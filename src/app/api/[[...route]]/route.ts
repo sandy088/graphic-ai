c1b3e825444ee82bee70a269fce2e80524e76f56
Sandeep <sandeeppakho55@gmail.com>
2025-11-22 17:35:54 +0530

    FIX Auth session problem

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -17,7 +17,7 @@ export const runtime = "nodejs";
 
 function getAuthConfig(c: Context): AuthConfig {
   return {
-    secret: c.env.AUTH_SECRET,
+    secret: process.env.AUTH_SECRET,
     ...authConfig,
   } as AuthConfig;
 }
